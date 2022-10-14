f7e26be06e475587f08265c1896c6f1ab5aee43b
Santom21 <angad.bhuyan@gmail.com>
2022-10-14 09:13:42 +0530

    test commit

---
+++ saving_model/pickle/test.py
@@ -0,0 +1,12 @@
+import pickle
+
+#load the model
+
+model = pickle.load(open('diabetes_79.pkl', 'rb'))
+
+result = model.predict([[1,1,1,1,1,1,1,1]])
+
+if result[0] == 1:
+    print("diabetic")
+else:
+    print("not diabetic")
